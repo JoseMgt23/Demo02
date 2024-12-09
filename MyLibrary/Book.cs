1f07f2f19100eda7aa46221bacf4e893126b93df
jose manuel gonzales trochez <joseclasef3ae@gmail.com>
2024-12-09 16:11:20 -0500

    Constructor Book

---
+++ MyLibrary/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Book : Product
+    {
+        public Book(int id, string name, decimal unitPrice, int unitsInStock, string title):
+            base(id, name, unitPrice, unitsInStock)
+        {
+
+        }
+    }
+}
