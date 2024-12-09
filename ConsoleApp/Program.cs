e8ebccb33a3bcecefce0e9479163c7b175ce8a44
jose manuel gonzales trochez <joseclasef3ae@gmail.com>
2024-12-09 16:08:21 -0500

    Creacion de un tipo de dato

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using MyLibrary;
+
+int i = 5;
+string Name = "Hello";
+
+Product Leche;
+Leche = new Product(1, "lecge entera", 19, 100);
--- MyLibrary/Product.cs
+++ MyLibrary/Product.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MyLibrary
 {
     public class Product(int id, string name, decimal unitPrice, int unitsInStock)
