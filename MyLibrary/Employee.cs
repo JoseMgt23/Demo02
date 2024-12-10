fb681a6a6edad5369bbc5aedcca65544e6673259
jose manuel gonzales trochez <joseclasef3ae@gmail.com>
2024-12-10 11:37:17 -0500

    Creacion de clase abstracta

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public abstract decimal CalculaMonthlyPayment();
+    }
+
+    public class Secretary : Employee
+    {
+        public override decimal CalculaMonthlyPayment() => 15000;
+        
+    }
+
+    public class Manager : Employee
+    {
+
+    }
+        
+}
